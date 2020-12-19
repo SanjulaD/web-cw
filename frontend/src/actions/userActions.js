2ea5ad445e1ab8859d2fb7b0fd917b6d69e7de6d
SanjulaD <desanjula99@gmail.com>
2020-12-19 21:53:13 +0530

    user in navbar and logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
+    USER_LOGOUT
 } from './../constants/userConstants.js'
 
 export const login = (email, password) => async (dispatch) => {
@@ -38,4 +39,11 @@ export const login = (email, password) => async (dispatch) => {
                     : error.message
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({
+        type: USER_LOGOUT
+    })
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css'
 
+import { logout } from './../../actions/userActions'
+
 const Header = () => {
+
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
 
     return (
         <Navbar collapseOnSelect expand="lg" fixed="top">
@@ -25,15 +37,26 @@ const Header = () => {
                     <LinkContainer to="/supplier">
                         <Nav.Link className="nav-cal">SUPPLIER</Nav.Link>
                     </LinkContainer>
-                    <LinkContainer to="/login">
-                        <Nav.Link className="login nav-cal">LOGIN</Nav.Link>
-                    </LinkContainer>
                     <LinkContainer to="/cart">
                         <Nav.Link className="cart nav-cal">
                             <i className="fas fa-shopping-cart"></i>
                             CART
                         </Nav.Link>
                     </LinkContainer>
+                    {
+                        userInfo ? (
+                            <NavDropdown title={userInfo.name.toUpperCase()} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>PROFILE</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>LOGOUT</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link className="login nav-cal">SIGN IN</Nav.Link>
+                                </LinkContainer>
+                            )
+                    }
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
