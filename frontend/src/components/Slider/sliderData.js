3678eea39ad9a78d53c119780d65328a2aba456d
SanjulaD <desanjula99@gmail.com>
2021-01-06 20:58:27 +0530

    slider changes #3

--- frontend/src/components/Slider/sliderData.js
+++ frontend/src/components/Slider/sliderData.js
@@ -3,19 +3,19 @@ const slider = [
         title: "Vulputate Mollis Ultricies Fermentum Parturient",
         description:
             "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
-        image: "https://i.imgur.com/ZXBtVw7.jpg",
+        image: "https://images.unsplash.com/photo-1574943320219-553eb213f72d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=682&q=80",
     },
     {
         title: "Tortor Dapibus Commodo Aenean Quam",
         description:
             "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-        image: "https://i.imgur.com/DCdBXcq.jpg",
+        image: "https://images.unsplash.com/photo-1592982537447-7440770cbfc9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1499&q=80",
     },
     {
         title: "Phasellus volutpat metus",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-        image: "https://i.imgur.com/DvmN8Hx.jpg",
+        image: "https://images.unsplash.com/photo-1523348837708-15d4a09cfac2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
     }
 ];
 
